35e46fb950a7b888afb1fa4190d26e64bb68f742
yinna <yn@bvrft.com>
2021-03-25 11:29:32 +0800

    feat: 页面优化

--- src/views/home/menu1_1.vue
+++ src/views/home/menu1_1.vue
@@ -49,6 +49,58 @@ export default {
     search() {
       this.fetchList()
     }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      class Node {
+        constructor(id) {
+          // id = 0,1,2...
+          this.id = id
+          this.neighbors = new Set()
+        }
+        connect(node) {
+          console.log('4', node, this)
+          if (node !== this) {
+            this.neighbors.add(node)
+            node.neighbors.add(this)
+          }
+        }
+      }
+      class RandomGraph {
+        constructor(size) {
+          // size = 6
+          this.nodes = new Set()
+          // 创建节点
+          for (let i = 0; i < size; ++i) {
+            this.nodes.add(new Node(i))
+          }
+          // 随机连接节点
+          const threshold = 1 / size
+          for (const x of this.nodes) {
+            for (const y of this.nodes) {
+              if (Math.random() < threshold) {
+                x.connect(y)
+              }
+            }
+          }
+        }
+        // 这个方法仅用于调试
+        // print() {
+        //   for (const node of this.nodes) {
+        //     const ids = [...node.neighbors]
+        //       .map((n) => n.id)
+        //       .join(',')
+        //     console.log(`${node.id}: ${ids}`)
+        //   }
+        // }
+      }
+      const g = new RandomGraph(6)
+      console.log(g)
+      // g.print()
+    }
   }
 }
 </script>
